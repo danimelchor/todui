389e0c997f6f8fbcbcbc32b47b3a50641e15c274
danimelchor <dmh672@gmail.com>
2023-03-13 17:40:40 -0400

    better list display with cli ls

--- src/cli/cli_utils.rs
+++ src/cli/cli_utils.rs
@@ -1,12 +1,12 @@
 use super::formats::Format;
-use crate::{configuration::Settings, task::Task};
+use crate::{configuration::Settings, task::Task, utils};
 
 pub fn print_task(task: &Task, format: Option<Format>, settings: &Settings) {
     match format {
         Some(Format::Json) => println!("{}", serde_json::to_string_pretty(&task).unwrap()),
         _ => {
             println!("{} {}", task.id.unwrap(), task.name);
-            println!("Date: {}", task.date);
+            println!("Date: {}", utils::date_to_display_str(&task.date, &settings));
             println!("Repeats: {:}", task.repeats);
             if let Some(description) = &task.description {
                 println!("Description: {}", description);
@@ -22,13 +22,13 @@ pub fn print_tasks(tasks: Vec<&Task>, format: Option<Format>, settings: &Setting
         _ => {
             for task in tasks {
                 let id = task.id.unwrap();
-                let name = &task.name;
-                let date = task.date;
+                let name = utils::ellipsis_or_pad(&task.name, 20);
+                let date = utils::date_to_display_str(&task.date, &settings);
                 let repeats = &task.repeats;
                 let complete = task.complete;
 
                 let x = settings.icons.get_icon(complete);
-                println!("{} {} ({}) {} {}", x, name, id, date, repeats);
+                println!("{} {} ({})\t{}\t{}", x, name, id, date, repeats);
             }
         }
     }
